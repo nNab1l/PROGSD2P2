849b97dc6bfb5fb5f1088a93f0d4b76540ec2307
Nabil <33580@ma-web.nl>
2022-11-30 12:53:45 +0100

    week 2 opdracht 1

---
+++ opdracht-2/private/init.php
@@ -0,0 +1,7 @@
+<?php
+define("PRIVATE_PATH", dirname(__FILE__));
+define("PROJECT_PATH", dirname(PRIVATE_PATH));
+define("SHARED_PATH", PRIVATE_PATH . "/shared");
+
+echo $_SERVER['SCRIPT_NAME']
+?>
