aaf666c74dfb7cfbe5e7d92302c278af858ee3dd
Nabil <33580@ma-web.nl>
2022-12-13 23:28:32 +0100

    week 3 opdracht 1

---
+++ opdracht-4/README.md
@@ -0,0 +1,7 @@
+# Wat doet stripslashes, addslashes, htmlspecialchars
+
+## stripslashes zorgen ervoor dat tekens zoals '/' weg worden gehaald
+
+## addslashes doet het tegenovergestelde van stripslashes en voegt dus backslashes toe. Dit kan gebruikt worden om sql injecties tegen te gaan
+
+## htmlspecialchars verandert sommige characters naar html codenamen de browser output verandert het terug naar html.
--- opdracht-4/voorbeeld.php
+++ opdracht-4/voorbeeld.php
@@ -1,18 +1,16 @@
 <?php
-// echo $_POST['email'];
+$email = "";
 $str = ' ongeldige gebruikersnaam ';
 if (!isset($_POST['user'], $_POST['comm'], $_POST['email'], $_POST['submit'])) {
 } else {
     if (empty(trim($_POST['user']))){
         echo htmlspecialchars($str);
     }
-
-    if (empty(trim($_POST['email']))){
-        echo ' ongeldig email ';
-    }
-
-    if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
-        echo ' Ongeldig email formaat ';
-    }
+    
+    if (filter_var(($_POST['email']), FILTER_VALIDATE_EMAIL)) {
+        echo("is a valid email address");
+      } else {
+        echo("is not a valid email address");
+      }
        
 }
